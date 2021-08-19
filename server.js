6693981eca7b83d957483cc21861d2920f74c85a
Mel Jack <melissa_jack@hotmail.co.uk>
2021-08-19 10:44:00 +0100

    Add server /notes and * routes

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const fs = require("fs");
+const path = require('path');
+
+const PORT = process.env.port || 3001;
+const app = express();
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+ 
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
