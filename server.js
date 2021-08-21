f10ff55b51808219a3c80117dcffcce1957d568e
Mel Jack <melissa_jack@hotmail.co.uk>
2021-08-21 21:03:00 +0100

    Add static assets route

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const fs = require("fs");
 const path = require('path');
+const notesData = require('./db/db.json');
 
 const PORT = process.env.port || 3001;
 const app = express();
+
+app.use(express.static("./public"));
+
+app.get('/api/notes', (req, res) => res.json(notesData));
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
