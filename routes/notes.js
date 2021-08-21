bd6881b44a51e87fa71407e7ba912c6271c61dd0
Mel Jack <melissa_jack@hotmail.co.uk>
2021-08-21 21:33:05 +0100

    Move /api/notes route to separate file

---
+++ routes/notes.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const fs = require('fs');
+const router = express.Router();
+const notesData = require('../db/db.json');
+const uuid = require('../helpers/uuid');
+
+router.get('/', (req, res) => res.json(notesData));
+
+router.post('/', (req, res) => {
+  console.info(`${req.method} request received to add a new note`);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully!`);
+  } else {
+    res.error('Error in adding note.');
+  }
+});
+
+const writeToFile = (destination, content) =>
+ fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+   err ? console.error(err) : console.info(`\nData written to ${destination}`)
+);
+ 
+const readAndAppend = (content, file) => {
+ fs.readFile(file, 'utf8', (err, data) => {
+   if (err) {
+     console.error(err);
+   } else {
+     const parsedData = JSON.parse(data);
+     parsedData.push(content);
+     writeToFile(file, parsedData);
+   }
+ });
+};
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require('path');
-const notesData = require('./db/db.json');
-const uuid = require('./helpers/uuid');
+const apiRoutes = require("./routes/notes.js");
 
 const PORT = process.env.port || 3001;
 const app = express();
@@ -11,27 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./public"));
-
-app.get('/api/notes', (req, res) => res.json(notesData));
-
-app.post('/api/notes', (req, res) => {
-  console.info(`${req.method} request received to add a new note`);
-
-  const { title, text } = req.body;
-
-  if (req.body) {
-    const newNote = {
-      title,
-      text,
-      note_id: uuid(),
-    };
-
-    readAndAppend(newNote, './db/db.json');
-    res.json(`Note added successfully!`);
-  } else {
-    res.error('Error in adding note.');
-  }
-});
+app.use("/api/notes", apiRoutes)
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
@@ -41,23 +20,6 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-
- const writeToFile = (destination, content) =>
- fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-   err ? console.error(err) : console.info(`\nData written to ${destination}`)
- );
- 
- const readAndAppend = (content, file) => {
- fs.readFile(file, 'utf8', (err, data) => {
-   if (err) {
-     console.error(err);
-   } else {
-     const parsedData = JSON.parse(data);
-     parsedData.push(content);
-     writeToFile(file, parsedData);
-   }
- });
- };
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
