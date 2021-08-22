b2832c8cb6026a42b687d16a51f244ea694e8487
Mel Jack <melissa_jack@hotmail.co.uk>
2021-08-22 11:15:34 +0100

    Small edit to app.listen

--- server.js
+++ server.js
@@ -21,5 +21,5 @@ app.get('*', (req, res) =>
 );
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+  console.log(`App listening at http://localhost:${PORT}`)
 );
